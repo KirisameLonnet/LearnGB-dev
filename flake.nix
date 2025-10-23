c60b8b02b1a5928df4a99915618e63c54bcb337d
lonnetkirisame <your-email@example.com>
2025-10-23 19:08:18 +0800

    update nixflake for macos

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A Nix Flake for Game Boy (GB/GBC) development on Linux";
+  description = "A Nix Flake for Game Boy (GB/GBC) development on Linux and arm64 macOS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -7,29 +7,30 @@
 
   outputs = { self, nixpkgs }:
     let
-
-      system = "x86_64-linux";
-
-
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            name = "gb-dev-shell";
 
-      devShells.${system}.default = pkgs.mkShell {
-        name = "gb-dev-shell";
+            buildInputs = with pkgs; [
+              rgbds
+            ];
 
-        buildInputs = with pkgs; [  
-          rgbds
-	  
-        ];
-
-        shellHook = ''
-          echo "--- Game Boy Development Environment ---"
-          echo "Tools available: lcc, rgbasm, emulicious-qt, make"
-          echo "--------------------------------------"
-        '';
-      };
+            shellHook = ''
+              echo "--- Game Boy Development Environment ---"
+              echo "Tools available: lcc, rgbasm, emulicious-qt, make"
+              echo "--------------------------------------"
+            '';
+          };
+        });
     };
 }
